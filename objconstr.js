e8b183eb90af87d422c55c3d9fe18226e680ac5e
Connie <connieyzhu3@gmail.com>
2023-09-12 13:26:25 -0700

    Fix deletion, change read status, small tweaks

--- objconstr.js
+++ objconstr.js
@@ -1,6 +1,8 @@
 const myLibrary = [];
+const cardLibrary = [];
 const libraryContainer = document.getElementById('library');
 let form = document.getElementById('addBookForm');
+let spanID = 0;
 
 function Book(title, author, pages, hasRead){
     this.title = title;
@@ -12,16 +14,6 @@ function Book(title, author, pages, hasRead){
     }
 }
 
-function displayLibrary(){
-  for(book in myLibrary){
-    console.log('book');
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.textContent = 'hi';
-    libraryContainer.appendChild(card);
-  }
-}
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -29,8 +21,6 @@ form.addEventListener('submit', (e) => {
   let author = document.getElementById('author').value;
   let pages = document.getElementById('pages').value;
   let hasRead = document.getElementById('read').checked;
-
-  console.log(hasRead);
 
   const book = new Book(title, author, pages, hasRead);
   addBookToLibrary(book);
@@ -42,17 +32,21 @@ function createCard(book){
   const card = document.createElement('div');
   card.className = 'card';
 
+  spanID++;
   const close = document.createElement('span');
+  close.className = 'close';
+  close.setAttribute('id', `${spanID}`)
+  close.setAttribute('onclick', 'deleteBook(this); return false;')
   close.innerHTML = '&times;';
 
   const bookTitle = document.createElement('h2');
   bookTitle.textContent = book.title;
 
   const bookAuthor = document.createElement('h3');
-  bookAuthor.textContent = book.author;
+  bookAuthor.textContent = 'By: ' + book.author;
 
   const pageNum = document.createElement('p');
-  pageNum.textContent = book.pages;
+  pageNum.textContent = book.pages + ' pgs';
 
   const readStatus = document.createElement('button');
   if(book.hasRead){
@@ -63,7 +57,10 @@ function createCard(book){
     readStatus.textContent = 'Not Read';
   }
 
+  readStatus.setAttribute('onclick', 'changeReadStatus(this); return false;')
+
   card.append(close, bookTitle, bookAuthor, pageNum, readStatus);
+  cardLibrary.push(card);
   return card;
 
 }
@@ -74,3 +71,32 @@ function addBookToLibrary(book){
   libraryContainer.appendChild(newBook);
 }
 
+function deleteBook(btn){
+  let nodeList = document.querySelectorAll('.card');
+  let btnNodeList = btn.parentNode;
+  let index;
+  console.log(btn.id);
+  for(i = 0; i < cardLibrary.length; i++){
+    if(cardLibrary[i].firstChild.id == btn.id){
+      index = i;
+    }
+  }
+  btn.parentNode.remove();
+  cardLibrary.splice(index, 1);
+  myLibrary.splice(index, 1);
+}
+
+function changeReadStatus(btn){
+  if(btn.className == 'read'){
+    btn.className = 'not_read';
+    btn.textContent = 'Not Read';
+  }else{
+    btn.className = 'read';
+    btn.textContent = 'Read';
+  }
+}
+
+const book1 = new Book ('num1', 'aut1', 123, true);
+const book2 = new Book ('num2', 'aut2', 124, false);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
