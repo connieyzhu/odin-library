d7bc8829cc80072f4226e7d2d6f2f503729f9984
Connie <connieyzhu3@gmail.com>
2023-09-11 21:27:04 -0700

    Connect form submission to button creation

--- objconstr.js
+++ objconstr.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const libraryContainer = document.getElementById('library');
+let form = document.getElementById('addBookForm');
 
 function Book(title, author, pages, hasRead){
     this.title = title;
@@ -11,8 +12,38 @@ function Book(title, author, pages, hasRead){
     }
 }
 
+function displayLibrary(){
+  for(book in myLibrary){
+    console.log('book');
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.textContent = 'hi';
+    libraryContainer.appendChild(card);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let hasRead = document.getElementById('read').checked;
+
+  console.log(hasRead);
+
+  const book = new Book(title, author, pages, hasRead);
+  addBookToLibrary(book);
+
+  form.reset();
+});
+
 function createCard(book){
   const card = document.createElement('div');
+  card.className = 'card';
+
+  const close = document.createElement('span');
+  close.innerHTML = '&times;';
 
   const bookTitle = document.createElement('h2');
   bookTitle.textContent = book.title;
@@ -32,26 +63,14 @@ function createCard(book){
     readStatus.textContent = 'Not Read';
   }
 
-  card.append(bookTitle, bookAuthor, pageNum, readStatus);
+  card.append(close, bookTitle, bookAuthor, pageNum, readStatus);
   return card;
 
 }
 
-function displayLibrary(){
-  for(book in myLibrary){
-    console.log('book');
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.textContent = 'hi';
-    libraryContainer.appendChild(card);
-  }
+function addBookToLibrary(book){
+  myLibrary.push(book);
+  let newBook = createCard(book);
+  libraryContainer.appendChild(newBook);
 }
 
-function addBookToLibrary(){
-  
-}
-
-/*const book1 = new Book('hi', 'hi', 100, true);
-myLibrary.push(book1);*/
-
-displayLibrary();
